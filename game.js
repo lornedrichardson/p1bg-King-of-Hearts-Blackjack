f0cbd6e3f9b0327edf534e8be1bbaf26d573b26b
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-06-01 21:05:08 -0700

    Basic game logic

---
+++ game.js
@@ -0,0 +1,113 @@
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+function buildDeck() {
+  var deck = new Array();
+  for (let i = 0 ; i < values.length; i++) {
+    for (let j = 0; j < suits.length; j++) {
+      let wt = parseInt(values[i]);
+      if (values[i] == "J" || values[i] == "Q" || values[i] == "K") wt = 10;
+      if (values[i] == "A") wt = 11;
+      let card = { value: values[i], suit: suits[j], weight: wt };
+      deck.push(card);
+    }
+  }
+}
+
+function shuffle() {
+  // for 1000 turns
+  // switch the values of two random cards
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor(Math.random() * deck.length);
+    let location2 = Math.floor(Math.random() * deck.length);
+    let tmp = deck[location1];
+    deck[location1] = deck[location2];
+    deck[location2] = tmp;
+  }
+}
+
+function addPlayers(numPlayers) {
+  var players = new Array();
+  for (let i = 1; i <= num; i++) {
+    let cards = new Array();
+    let player = { name: 'Player ' + i, id: i, chips: 0, hand: cards };
+    players.push(player);
+  }
+}
+
+function dealHands() {
+  // alternate handing cards to each player
+  // 2 cards each
+  for (let i = 0; i < 2; i++) {
+    for (let x = 0; x < players.length; x++) {
+      let card = deck.pop();
+      players[x].hand.push(card);
+      renderCard(card, x);
+      updateHandValue();
+    }
+  }
+
+  updateDeck();
+}
+
+function updateHandValue() {
+  for (var i = 0 ; i < players.length; i++) {
+    getHandValue(i);
+    document.getElementById('Hand_value_' + i).innerHTML = players[i].handValue;
+  }
+}
+
+function getHandValue(player) {
+  let total = 0;
+  for (let i = 0; i < players[player].hand.length; i++) total += players[player].hand[i].weight;
+  players[player].handValue = total;
+  return total;
+}
+
+function hit() {
+  // pop a card from the deck to the current player
+  // check if current player new points are over 21
+  let card = deck.pop();
+  players[currentPlayer].Hand.push(card);
+  renderCard(card, currentPlayer);
+  updateHandValue();
+  updateDeck();
+  check();
+}
+
+function stand() {
+  // move on to next player
+  if (currentPlayer != players.length - 1) {
+    document.getElementById('player_' + currentPlayer).classList.remove('active');
+    currentPlayer += 1;
+    document.getElementById('player_' + currentPlayer).classList.add('active');
+  } else end();
+}
+
+function end() {
+  let winner = -1;
+  let score = 0;
+
+  for (let i = 0; i < players.length - 1; i++) {
+    if (players[i].handValue > players[players.length - 1].handValue && players[i].handValue < 22) winner = i;
+    score = players[i].handValue;
+  }
+  document.getElementById('status').innerHTML = 'Winner: Player ' + players[winner].id;
+  document.getElementById("status").style.display = "inline-block";
+}
+
+function compareHandValues() {
+  if (players[currentPlayer].handValue > 21) document.getElementById('status').innerHTML = 'Player: ' + players[currentPlayer].id + ' Busted';
+  document.getElementById('status').style.display = "inline-block";
+  end();
+}
+
+function updateDeck() {
+  document.getElementById('deckcount').innerHTML = deck.length;
+}
+
+window.addEventListener('load', () => {
+  buildDeck();
+  shuffle();
+  addPlayers(2);
+});
