788e54a923cd260c55d870948776bb90d38ff7b6
Lorne Richardson <46414696+lornedrichardson@users.noreply.github.com>
2023-06-09 20:51:56 -0700

    Resolving the ace-logic and double-button issues

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 /** @format */
 
+// Constants
 const suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
 const values = [
 	'2',
@@ -16,344 +17,173 @@ const values = [
 	'K',
 	'A',
 ];
+
+// Game variables
 let deck = [];
-let playedCards = [];
 let playerHand = [];
 let dealerHand = [];
 let playerHandValue = 0;
 let dealerHandValue = 0;
-let winnings = 0;
-let bet = 10;
+let gameOver = false;
 
+// DOM elements
+const playerHandElement = document.getElementById('player-hand');
+const dealerHandElement = document.getElementById('dealer-hand');
+const messageElement = document.getElementById('message');
+
+// Build the deck array of card objects
+function buildDeck() {
+	for (let suit of suits) {
+		for (let value of values) {
+			let weight = parseInt(value);
+			if (value === 'J' || value === 'Q' || value === 'K') {
+				weight = 10;
+			} else if (value === 'A') {
+				weight = 11;
+			}
+			deck.push({ suit, value, weight });
+		}
+	}
+}
+
+// Shuffle the deck using Fisher-Yates algorithm
+function shuffle() {
+	for (let i = deck.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		[deck[i], deck[j]] = [deck[j], deck[i]];
+	}
+}
+
+// Create a DOM element for a card
 function createCardElement(card) {
 	const cardElement = document.createElement('div');
-	cardElement.className = 'card';
-
-	const cardValueElement = document.createElement('div');
-	cardValueElement.className = 'card-value';
-	cardValueElement.textContent = card.value;
-
-	const cardSuitElement = document.createElement('div');
-	cardSuitElement.className = 'card-suit';
-	cardSuitElement.textContent = getSuitSymbol(card.suit);
-
-	const cardImageElement = document.createElement('img');
-	cardImageElement.className = 'card-image';
-	cardImageElement.src = getCardImage(card);
-	cardImageElement.alt = `${card.value} of ${card.suit}`;
-
-	cardElement.appendChild(cardValueElement);
-	cardElement.appendChild(cardSuitElement);
-	cardElement.appendChild(cardImageElement);
-
+	cardElement.classList.add('card');
+	const valueElement = document.createElement('div');
+	valueElement.classList.add('value');
+	valueElement.textContent = card.value;
+	const suitElement = document.createElement('div');
+	suitElement.classList.add('suit');
+	suitElement.textContent = getSuitSymbol(card.suit);
+	cardElement.appendChild(valueElement);
+	cardElement.appendChild(suitElement);
 	return cardElement;
 }
 
-function getCardImage(card) {
-	let cardImage = '';
-	if (card.suit === 'Spades') {
-		if (card.value === '2') cardImage = 'images/spade_2.png';
-		if (card.value === '3') cardImage = 'images/spade_3.png';
-		if (card.value === '4') cardImage = 'images/spade_4.png';
-		if (card.value === '5') cardImage = 'images/spade_5.png';
-		if (card.value === '6') cardImage = 'images/spade_6.png';
-		if (card.value === '7') cardImage = 'images/spade_7.png';
-		if (card.value === '8') cardImage = 'images/spade_8.png';
-		if (card.value === '9') cardImage = 'images/spade_9.png';
-		if (card.value === '10') cardImage = 'images/spade_10.png';
-		if (card.value === 'J') cardImage = 'images/spade_jack.png';
-		if (card.value === 'Q') cardImage = 'images/spade_queen.png';
-		if (card.value === 'K') cardImage = 'images/spade_king.png';
-		if (card.value === 'A') cardImage = 'images/spade_1.png';
-	} else if (card.suit === 'Hearts') {
-		if (card.value === '2') cardImage = 'images/heart_2.png';
-		if (card.value === '3') cardImage = 'images/heart_3.png';
-		if (card.value === '4') cardImage = 'images/heart_4.png';
-		if (card.value === '5') cardImage = 'images/heart_5.png';
-		if (card.value === '6') cardImage = 'images/heart_6.png';
-		if (card.value === '7') cardImage = 'images/heart_7.png';
-		if (card.value === '8') cardImage = 'images/heart_8.png';
-		if (card.value === '9') cardImage = 'images/heart_9.png';
-		if (card.value === '10') cardImage = 'images/heart_10.png';
-		if (card.value === 'J') cardImage = 'images/heart_jack.png';
-		if (card.value === 'Q') cardImage = 'images/heart_queen.png';
-		if (card.value === 'K') cardImage = 'images/heart_king.png';
-		if (card.value === 'A') cardImage = 'images/heart_1.png';
-	} else if (card.suit === 'Diamonds') {
-		if (card.value === '2') cardImage = 'images/diamond_2.png';
-		if (card.value === '3') cardImage = 'images/diamond_3.png';
-		if (card.value === '4') cardImage = 'images/diamond_4.png';
-		if (card.value === '5') cardImage = 'images/diamond_5.png';
-		if (card.value === '6') cardImage = 'images/diamond_6.png';
-		if (card.value === '7') cardImage = 'images/diamond_7.png';
-		if (card.value === '8') cardImage = 'images/diamond_8.png';
-		if (card.value === '9') cardImage = 'images/diamond_9.png';
-		if (card.value === '10') cardImage = 'images/diamond_10.png';
-		if (card.value === 'J') cardImage = 'images/diamond_jack.png';
-		if (card.value === 'Q') cardImage = 'images/diamond_queen.png';
-		if (card.value === 'K') cardImage = 'images/diamond_king.png';
-		if (card.value === 'A') cardImage = 'images/diamond_1.png';
-	} else if (card.suit === 'Clubs') {
-		if (card.value === '2') cardImage = 'images/club_2.png';
-		if (card.value === '3') cardImage = 'images/club_3.png';
-		if (card.value === '4') cardImage = 'images/club_4.png';
-		if (card.value === '5') cardImage = 'images/club_5.png';
-		if (card.value === '6') cardImage = 'images/club_6.png';
-		if (card.value === '7') cardImage = 'images/club_7.png';
-		if (card.value === '8') cardImage = 'images/club_8.png';
-		if (card.value === '9') cardImage = 'images/club_9.png';
-		if (card.value === '10') cardImage = 'images/club_10.png';
-		if (card.value === 'J') cardImage = 'images/club_jack.png';
-		if (card.value === 'Q') cardImage = 'images/club_queen.png';
-		if (card.value === 'K') cardImage = 'images/club_king.png';
-		if (card.value === 'A') cardImage = 'images/club_1.png';
-	}
-	return cardImage;
-}
-
-function buildDeck() {
-	for (let i = 0; i < values.length; i++) {
-		for (let j = 0; j < suits.length; j++) {
-			let wt = parseInt(values[i]);
-			if (values[i] === 'J' || values[i] === 'Q' || values[i] === 'K') wt = 10;
-			if (values[i] === 'A') wt = 11;
-			let card = { value: values[i], suit: suits[j], weight: wt };
-			deck.push(card);
-		}
+// Get the HTML entity for a suit symbol
+function getSuitSymbol(suit) {
+	switch (suit) {
+		case 'Spades':
+			return '♠︎';
+		case 'Hearts':
+			return '♥︎';
+		case 'Diamonds':
+			return '♦︎';
+		case 'Clubs':
+			return '♣︎';
+		default:
+			return '';
 	}
 }
 
-function shuffle() {
-	// switch the values of two random cards 1000 times
-	for (let i = 0; i < 1000; i++) {
-		let location1 = Math.floor(Math.random() * deck.length);
-		let location2 = Math.floor(Math.random() * deck.length);
-		let tmp = deck[location1];
-		deck[location1] = deck[location2];
-		deck[location2] = tmp;
-	}
-}
-
-function dealHands() {
-	for (let i = 0; i < 2; i++) {
-		dealCard('player');
-		dealCard('dealer');
-	}
-	if (playerHandValue === 21 && dealerHandValue !== 21) {
-		gameResult('player');
-	} else if (playerHandValue === 21 && dealerHandValue === 21) {
-		gameResult('push');
-	} else if (playerHandValue !== 21 && dealerHandValue === 21) {
-		gameResult('dealer');
-	}
-}
-
-function dealCard(who) {
+// Deal a card to a hand and update the DOM
+function dealCard(hand, handElement) {
 	const card = deck.pop();
-	playedCards.push(card);
-	if (deck.length === 0) return 'error';
-	if (who === 'player') {
-		playerHandValue += card.weight;
-		playerHand.push(card);
-	} else {
-		dealerHandValue += card.weight;
-		dealerHand.push(card);
-	}
+	hand.push(card);
+	const cardElement = createCardElement(card);
+	handElement.appendChild(cardElement);
 }
 
+// Calculate the value of a hand
+function calculateHandValue(hand) {
+	let value = 0;
+	let numAces = 0;
+	for (let card of hand) {
+		value += card.weight;
+		if (card.value === 'A') {
+			numAces++;
+		}
+	}
+	while (value > 21 && numAces > 0) {
+		value -= 10;
+		numAces--;
+	}
+	return value;
+}
+
+// Handle player hitting
 function hit() {
-	if (playerHandValue <= 21) {
-		dealCard('player');
-		adjustAceValue('player'); // Change function name to adjustAceValue
-		if (playerHandValue > 21) {
-			gameResult('dealer');
-		}
-		updateUI();
-	}
-}
-function adjustAceValue(who) {
-	if (who === 'player') {
-		let aces = playerHand.filter((card) => card.value === 'A');
-		if (aces.length > 0) {
-			for (let i = 0; i < aces.length; i++) {
-				if (playerHandValue > 21) {
-					playerHandValue -= 10;
-				}
-			}
-		}
-	} else if (who === 'dealer') {
-		let aces = dealerHand.filter((card) => card.value === 'A');
-		if (aces.length > 0) {
-			for (let i = 0; i < aces.length; i++) {
-				if (dealerHandValue > 21) {
-					dealerHandValue -= 10;
-				}
-			}
-		}
+	if (gameOver) return;
+	dealCard(playerHand, playerHandElement);
+	playerHandValue = calculateHandValue(playerHand);
+	if (playerHandValue > 21) {
+		endGame('You busted! You lose.');
 	}
 }
 
+// Handle player standing
 function stand() {
+	if (gameOver) return;
 	while (dealerHandValue < 17) {
-		dealCard('dealer');
-		adjustAceValue('dealer'); // Change function name to adjustAceValue
+		dealCard(dealerHand, dealerHandElement);
+		dealerHandValue = calculateHandValue(dealerHand);
 	}
 	if (dealerHandValue > 21) {
-		gameResult('player');
+		endGame('Dealer busted! You win!');
+	} else if (dealerHandValue === playerHandValue) {
+		endGame("Push! It's a tie.");
+	} else if (dealerHandValue > playerHandValue) {
+		endGame('You lose.');
 	} else {
-		compareHands(playerHandValue, dealerHandValue);
+		endGame('You win!');
 	}
-	updateUI();
 }
 
-function newHand() {
-	if (deck.length < 20) {
-		deck = deck.concat(playedCards);
-		playedCards = [];
-		shuffle();
-	}
+// Handle player doubling
+function double() {
+	hit();
+	stand();
+}
+
+// End the game
+function endGame(message) {
+	gameOver = true;
+	messageElement.textContent = message;
+}
+
+// Initialize the game
+function initializeGame() {
+	deck = [];
 	playerHand = [];
 	dealerHand = [];
 	playerHandValue = 0;
 	dealerHandValue = 0;
-	clearGame();
-	dealHands();
-	winnings = 0;
-	bet = 10;
-	document.getElementById('winnings').textContent = 'Winnings: ' + winnings;
-	document.getElementById('player-hand-value').textContent =
-		'Player Hand Value: ';
-	document.getElementById('dealer-hand-value').textContent =
-		'Dealer Hand Value: ';
-}
-function checkScore() {
-	if (playerHandValue > 21) {
-		let aces = playerHand.filter((card) => card.value === 'A');
-		if (aces.length > 0) {
-			for (let i = 0; i < aces.length; i++) {
-				playerHandValue -= 10;
-				if (playerHandValue <= 21) {
-					break;
-				}
-			}
-		}
-	}
+	gameOver = false;
+	playerHandElement.innerHTML = '';
+	dealerHandElement.innerHTML = '';
+	messageElement.textContent = '';
 
-	if (dealerHandValue > 21) {
-		let aces = dealerHand.filter((card) => card.value === 'A');
-		if (aces.length > 0) {
-			for (let i = 0; i < aces.length; i++) {
-				dealerHandValue -= 10;
-				if (dealerHandValue <= 21) {
-					break;
-				}
-			}
-		}
+	buildDeck();
+	shuffle();
+
+	dealCard(playerHand, playerHandElement);
+	dealCard(playerHand, playerHandElement);
+	playerHandValue = calculateHandValue(playerHand);
+
+	dealCard(dealerHand, dealerHandElement);
+	dealerHandValue = calculateHandValue(dealerHand);
+
+	if (playerHandValue === 21) {
+		endGame('Blackjack! You win!');
 	}
 }
 
-function gameResult(result) {
-	let message = '';
-	if (result === 'player') {
-		message = 'You win!';
-		winnings += bet;
-	} else if (result === 'dealer') {
-		message = 'Dealer wins!';
-		winnings -= bet;
-	} else {
-		message = "Push! It's a tie.";
-	}
-	document.getElementById('result').textContent = message;
-	document.getElementById('winnings').textContent = 'Winnings: ' + winnings;
-	document.getElementById('hit-btn').disabled = true;
-	document.getElementById('stand-btn').disabled = true;
-	document.getElementById('dbl-btn').disabled = true;
-}
-
-function clearGame() {
-	const playerHandElement = document.getElementById('player-hand');
-	const dealerHandElement = document.getElementById('dealer-hand');
-	const resultElement = document.getElementById('result');
-
-	while (playerHandElement.firstChild) {
-		playerHandElement.firstChild.remove();
-	}
-
-	while (dealerHandElement.firstChild) {
-		dealerHandElement.firstChild.remove();
-	}
-
-	resultElement.textContent = '';
-
-	document.getElementById('hit-btn').disabled = false;
-	document.getElementById('stand-btn').disabled = false;
-}
-function renderGame() {
-	const playerHandElement = document.getElementById('player-hand');
-	const dealerHandElement = document.getElementById('dealer-hand');
-
-	// Clear previous cards
-	while (playerHandElement.firstChild) {
-		playerHandElement.firstChild.remove();
-	}
-
-	while (dealerHandElement.firstChild) {
-		dealerHandElement.firstChild.remove();
-	}
-
-	// Render player's hand
-	playerHand.forEach((card) => {
-		const cardElement = createCardElement(card);
-		playerHandElement.appendChild(cardElement);
-	});
-
-	// Render dealer's hand
-	dealerHand.forEach((card) => {
-		const cardElement = createCardElement(card);
-		dealerHandElement.appendChild(cardElement);
-	});
-
-	// Display player's hand value
-	const playerHandValueElement = document.getElementById('player-hand-value');
-	playerHandValueElement.textContent = 'Player Hand Value: ' + playerHandValue;
-
-	// Hide the dealer's second card if the game is still in progress
-	if (playerHand.length === 2 && dealerHand.length === 2) {
-		dealerHandElement.children[1].style.display = 'none';
-	}
-
-	// Display dealer's hand value
-	const dealerHandValueElement = document.getElementById('dealer-hand-value');
-	dealerHandValueElement.textContent = 'Dealer Hand Value: ' + dealerHandValue;
-}
-
-function getSuitSymbol(suit) {
-	if (suit === 'Spades') return '♠';
-	if (suit === 'Hearts') return '♥';
-	if (suit === 'Diamonds') return '♦';
-	if (suit === 'Clubs') return '♣';
-}
-
-buildDeck();
-shuffle();
-dealHands();
-renderGame();
-
+// Event listeners
 document.getElementById('hit-btn').addEventListener('click', hit);
 document.getElementById('stand-btn').addEventListener('click', stand);
-document.getElementById('new-hand-btn').addEventListener('click', newHand);
+document.getElementById('dbl-btn').addEventListener('click', double);
+document
+	.getElementById('restart-btn')
+	.addEventListener('click', initializeGame);
 
-function compareHands(playerValue, dealerValue) {
-	if (playerValue > dealerValue) {
-		gameResult('player');
-	} else if (playerValue < dealerValue) {
-		gameResult('dealer');
-	} else {
-		gameResult('push');
-	}
-}
-
-function updateUI() {
-	renderGame();
-	checkScore();
-}
+// Initialize the game on page load
+initializeGame();
